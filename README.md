d1b86cb9c40052d6239bdc2499d7cd6763fb9bd8
Karthik <krthkv26@gmail.com>
2016-02-14 23:32:58 +0530

    the rest of it

---
+++ README.md
@@ -0,0 +1,12 @@
+# xkcd-936
+
+[![xkcd-936](http://imgs.xkcd.com/comics/password_strength.png)](https://xkcd.com/936/)
+
+### Does 👆🏾 in `rust`.
+
+* Clone
+* `cargo run`
+
+
+### License
+MIT
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod words;
+extern crate rand;
+
+use rand::{thread_rng, Rng};
+
+fn main(){
+    // generate random number generator
+    let mut rng = thread_rng();
+
+    // a mutable list of strings we've selected as random words
+    let mut random_words : Vec<&str> = vec![]; 
+
+    loop{
+        // stop looping if we already have 4 words
+        if random_words.len() == 4{
+            break;
+        }
+        // get a random number between 0 and len() of WORDS
+        let i: usize = rng.gen_range(0,words::WORDS.len());
+        // get the word at the index
+        let word: &str = words::WORDS[i];
+
+        // check if the word we just got is part of the list already
+        // continue looping if so
+        if random_words.contains(&word){
+            continue;
+        }
+        // add it to the array otherwise
+        random_words.push(&word);
+    }
+
+    // join and print
+    println!("{}",random_words.join(""));
+}
