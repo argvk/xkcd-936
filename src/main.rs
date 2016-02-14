5867a2e0bac1c7493c1312e90a2bd39a5b79f95f
Karthik <krthkv26@gmail.com>
2016-02-15 00:59:00 +0530

    drop unnecessary type definitions, compiler infers this
    from : https://twitter.com/cryze107/status/698949716185391109

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main(){
     let mut rng = thread_rng();
 
     // a mutable list of strings we've selected as random words
-    let mut random_words : Vec<&str> = vec![]; 
+    let mut random_words = vec![]; 
 
     loop{
         // stop looping if we already have 4 words
@@ -16,9 +16,9 @@ fn main(){
             break;
         }
         // get a random number between 0 and len() of WORDS
-        let i: usize = rng.gen_range(0,words::WORDS.len());
+        let i = rng.gen_range(0,words::WORDS.len());
         // get the word at the index
-        let word: &str = words::WORDS[i];
+        let word = words::WORDS[i];
 
         // check if the word we just got is part of the list already
         // continue looping if so
